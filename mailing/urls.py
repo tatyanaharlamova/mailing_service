872a49258221eb8ae59c7f392ebbc310d32bdf5f
Tatyana Kharlamova <kharlamova2785@gmail.com>
2024-05-27 22:07:48 +0300

    Create models

--- mailing/urls.py
+++ mailing/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from mailing.apps import MailingConfig
+from mailing.views import index, ClientListView, ClientCreateView, ClientDeleteView, ClientUpdateView, ClientDetailView
 
 app_name = MailingConfig.name
 
 urlpatterns = [
-    path('', ),
+    path('', index, name='index'),
+    path('create/', ClientCreateView.as_view(), name='create'),
+    path('clients_list/', ClientListView.as_view(), name='clients_list'),
+    path('delete/<int:pk>/', ClientDeleteView.as_view(), name='delete'),
+    path('edit/<int:pk>/', ClientUpdateView.as_view(), name='edit'),
+    path('view/<int:pk>/', ClientDetailView.as_view(), name='view'),
 ]
