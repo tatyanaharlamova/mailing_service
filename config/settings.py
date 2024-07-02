c97a81a2495cd064dfb8921dbf45b82519f67862
Tatyana Kharlamova <kharlamova2785@gmail.com>
2024-07-02 13:39:59 +0300

    Change send_mailing

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'mailing',
     'blog',
     'users',
+    'django_apscheduler',
 ]
 
 MIDDLEWARE = [
@@ -110,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
--- mailing/models.py
+++ mailing/models.py
@@ -10,11 +10,9 @@ class Client(models.Model):
     Модель для хранения информации о клиенте
     """
 
-    name = models.CharField(max_length=100, verbose_name="Имя")
-    email = models.EmailField(
-        max_length=100, verbose_name="Электронная почта", unique=True
-    )
-    comment = models.TextField(verbose_name="Комментарий", **NULLABLE)
+    name = models.CharField(max_length=100, verbose_name='Имя')
+    email = models.EmailField(max_length=100, verbose_name='Электронная почта')
+    comment = models.TextField(verbose_name='Комментарий', **NULLABLE)
     owner = models.ForeignKey(User, verbose_name='Владелец', on_delete=models.SET_NULL, **NULLABLE)
 
     def __str__(self):
@@ -48,13 +46,11 @@ class Mailing(models.Model):
     Модель для хранения информации о рассылке
     """
 
-    FIVE_MINUTES = "Каждые пять минут"
     DAILY = "Раз в день"
     WEEKLY = "Раз в неделю"
     MONTHLY = "Раз в месяц"
 
     PERIODICITY_CHOICES = [
-        (FIVE_MINUTES, "Каждые пять минут"),
         (DAILY, "Раз в день"),
         (WEEKLY, "Раз в неделю"),
         (MONTHLY, "Раз в месяц"),
@@ -71,12 +67,8 @@ class Mailing(models.Model):
     ]
 
     name = models.CharField(max_length=150, verbose_name="Название")
-    description = models.TextField(
-        **NULLABLE, verbose_name="Описание", help_text="не обязательное поле"
-    )
-    status = models.CharField(
-        max_length=150, choices=STATUS_CHOICES, default=CREATED, verbose_name="Статус"
-    )
+    description = models.TextField(**NULLABLE, verbose_name="Описание", help_text="не обязательное поле")
+    status = models.CharField(max_length=150, choices=STATUS_CHOICES, default=CREATED, verbose_name="Статус")
     periodicity = models.CharField(
         max_length=150,
         choices=PERIODICITY_CHOICES,
@@ -88,16 +80,19 @@ class Mailing(models.Model):
         **NULLABLE,
         help_text="(формат 11.05.2024) не обязательное поле",
     )
-    end_date = models.DateTimeField(
-        verbose_name="Дата окончания", **NULLABLE, help_text="не обязательное поле"
-    )
-
-    clients = models.ManyToManyField(Client, related_name='mailing', verbose_name="Клиенты для рассылки")
+    end_date = models.DateTimeField(verbose_name='Дата окончания', **NULLABLE, help_text='не обязательное поле')
+    next_send_time = models.DateTimeField(verbose_name='Время следующей отправки', **NULLABLE)
+    clients = models.ManyToManyField(Client, related_name='mailing', verbose_name='Клиенты для рассылки')
     message = models.ForeignKey(Message, verbose_name='Cообщение', on_delete=models.CASCADE, **NULLABLE)
     owner = models.ForeignKey(User, verbose_name='Владелец',  on_delete=models.SET_NULL, **NULLABLE)
 
     def __str__(self):
         return f"{self.name}, статус: {self.status}"
+
+    def save(self, *args, **kwargs):
+        if not self.next_send_time:
+            self.next_send_time = self.start_date
+        super().save(*args, **kwargs)
 
     class Meta:
         verbose_name = "Рассылка"
@@ -113,11 +108,17 @@ class Log(models.Model):
     """
     Модель для хранения информации о попытках рассылок
     """
+    SUCCESS = 'Успешно'
+    FAIL = 'Неуспешно'
+    STATUS_VARIANTS = [
+        (SUCCESS, 'Успешно'),
+        (FAIL, 'Неуспешно'),
+    ]
 
     time = models.DateTimeField(
         verbose_name="Дата и время попытки отправки", auto_now_add=True
     )
-    status = models.BooleanField(verbose_name="Статус попытки отправки")
+    status = models.CharField(max_length=50, choices=STATUS_VARIANTS, verbose_name='Cтатус рассылки')
     server_response = models.CharField(
         max_length=150, verbose_name="Ответ сервера почтового сервиса", **NULLABLE
     )
